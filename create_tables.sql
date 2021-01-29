aed8db43b95856c34d95b0244347f7175347d5be
Anderson Mills <nodog@konfuzo.net>
2021-01-29 13:49:16 -0600

    Write data to corrected db

--- create_tables.sql
+++ create_tables.sql
@@ -1,28 +1,28 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id VARCHAR(255) PRIMARY KEY,
-    billing_address VARCHAR(255),
-    mailing_address VARCHAR(255),
+    billing_address_id VARCHAR(255),
+    mailing_address_id VARCHAR(255),
     created_date TIMESTAMP NOT NULL,
     close_date TIMESTAMP,
     email__c VARCHAR(255),
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     phone VARCHAR(255),
+    plan_name VARCHAR(255),
     carrier VARCHAR(255), 
-    planName VARCHAR(255),
     premium  INT,
     product VARCHAR(255),
-    effective_date TIMESTAMP,
-    expiration_date TIMESTAMP,
+    effective_date__c TIMESTAMP,
+    expiration_date__c TIMESTAMP,
     stage VARCHAR(255),
-    CONSTRAINT fk_billing_address
-        FOREIGN KEY (billing_address)
+    CONSTRAINT fk_billing_address_id
+        FOREIGN KEY (billing_address_id)
             REFERENCES addresses (address_id)
             ON DELETE SET NULL,
-    CONSTRAINT fk_mailing_address
-        FOREIGN KEY (mailing_address)
+    CONSTRAINT fk_mailing_address_id
+        FOREIGN KEY (mailing_address_id)
             REFERENCES addresses (address_id)
-            ON DELETE SET NULL,
+            ON DELETE SET NULL
 );
 
 -- carrier, product, and stage would be foreign keys to tables not in this db
--- database_practice.py
+++ database_practice.py
@@ -1,3 +1,4 @@
+import uuid
 import psycopg2
 from psycopg2 import OperationalError
 
@@ -17,12 +18,12 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
     return connection
 
 def put_line_in_db(db_connection, line_items):
-    
-    [ billing_address,
+
+    [ billing_street,
     billing_state,
     billing_zip,
     billing_city,
-    mailing_address,
+    mailing_street,
     mailing_state,
     mailing_zip, 
     mailing_city,
@@ -40,22 +41,66 @@ def put_line_in_db(db_connection, line_items):
     expiration_date__c,
     stage ] = line_items
     
-    print(stage)
-    return None
+    premium_cents = int(premium.replace(".", ""))
+
+    billing_address_id = str(uuid.uuid1())
+    mailing_address_id = str(uuid.uuid1())
+
+    billing_insert_query = (
+        f"INSERT INTO addresses (address_id, street, city, zip, state) "
+        f"VALUES ('{billing_address_id}', '{billing_street}', '{billing_city}', '{billing_zip}', '{billing_state}')"
+        )
+    mailing_insert_query = (
+        f"INSERT INTO addresses (address_id, street, city, zip, state) "
+        f"VALUES ('{mailing_address_id}', '{mailing_street}', '{mailing_city}', '{mailing_zip}', '{mailing_state}')"
+        )
+
+    user_id = str(uuid.uuid4())
+
+    # print(created_date)
+    # print(premium_cents)
+
+    user_insert_query = (
+        f"INSERT INTO users (user_id, billing_address_id, mailing_address_id, created_date, "
+        f"close_date, email__c, first_name, last_name, "
+        f"phone, plan_name, carrier, premium, product, effective_date__c, expiration_date__c, stage) "
+        f"VALUES ('{user_id}', '{billing_address_id}', '{mailing_address_id}', '{created_date}', "
+        f"'{close_date}', '{email__c}', '{first_name}', '{last_name}', '{phone}', '{plan_name}', "
+        f"'{carrier}', '{premium_cents}', '{product}', '{effective_date__c}', '{expiration_date__c}', '{stage}')"
+    )
+
+    db_connection.autocommit = True
+    cursor = db_connection.cursor()
+    cursor.execute(billing_insert_query)
+    cursor.execute(mailing_insert_query)
+    cursor.execute(user_insert_query)
+
+    # print(first_name)
+    return True
 
 def read_all_that_data(db_connection):
 
     f = open("./interview_test_data_small.csv", "r")
 
+    count = 0
     for line in f:
 
-        line_items = line.split(sep=',')
+        quote_fix_line = line.replace("'", "''")
+        line_items = quote_fix_line.split(sep=',')
+        if line_items[0] == "BillingAddress":
+            continue
         put_line_in_db(db_connection, line_items)
     
+        count += 1
+
     f.close
+
+    return count
+
 
 if __name__ == "__main__":
     connection = create_connection(
         "practice", "servacct", "prop9613velo", "127.0.0.1", "5432"
     )
-    read_all_that_data(connection)
+    count = read_all_that_data(connection)
+    print (f"{count} lines read")
